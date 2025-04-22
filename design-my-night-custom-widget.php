c4a57e074f43cbfba2e2db64489fcb49fc77925c
ljsherlock <lewis@ljsherlock.com>
2025-04-22 13:28:06 +0700

    All changes requested implmented.

--- design-my-night-custom-widget.php
+++ design-my-night-custom-widget.php
@@ -14,21 +14,7 @@ if (! class_exists('Design_My_Night')) {
 	class Design_My_Night {
 		private $booking_data;
 		private $url;
-		private $times = [ 
-			'09:00-11:00',
-			'10:00-12:00',
-			'11:00-13:00',
-			'12:00-14:00',
-			'13:00-15:00',
-			'14:00-16:00',
-			'15:00-17:00',
-			'16:00-18:00',
-			'17:00-19:00',
-			'18:00-20:00',
-			'19:00-21:00',
-			'20:00-22:00',
-			'21:00-23:00'
-		];
+		private $times = [];
 
 		public function __construct()
 		{
@@ -41,12 +27,12 @@ if (! class_exists('Design_My_Night')) {
 			// Register shortcodes
 			add_shortcode('book_your_visit', array($this, 'book_your_visit_form'));
 			add_shortcode('private_hire', array($this, 'private_hire_form'));
+
+			$this->times = $this->hoursRange( 28800, 86400, 60 * 30, 'g:ia' );
+			$this->times_server = $this->hoursRange( 28800, 86400, 60 * 30, 'H:i' );
 		}
 
-		private function load_booking_data()
-		{
-			// $json_file_path = plugin_dir_path(__FILE__) . 'make-my-night-venue.json';
-			// use API here (ljsherlock)
+		private function load_booking_data() {
 
 			$DesignMyNight = new DesignMyNight();
 
@@ -256,7 +242,7 @@ if (! class_exists('Design_My_Night')) {
 									<path fill-rule="evenodd" clip-rule="evenodd" d="M12.6939 12.6375C11.8834 12.2297 10.9679 12 10 12C6.68629 12 4 14.6863 4 18V20H16V18C16 17.6596 15.9716 17.3255 15.9169 17H20V14.75C20 12.6235 18.1515 11 16 11C14.6441 11 13.422 11.6366 12.6939 12.6375ZM13.9407 13.4754C14.5786 14.0315 15.0981 14.7205 15.4558 15.5H18.5V14.75C18.5 13.5628 17.4384 12.5 16 12.5C15.1288 12.5 14.3841 12.8975 13.9407 13.4754ZM10 13.5C7.51472 13.5 5.5 15.5147 5.5 18V18.5H14.5V18C14.5 17.4727 14.4096 16.9681 14.2441 16.4999C13.9244 15.5953 13.3224 14.8219 12.5431 14.2869C11.8199 13.7905 10.945 13.5 10 13.5Z" fill="#fff" />
 								</svg>
 							</label>
-							<input type="number" name="num_people" id="num_people" required/>
+							<input type="number" name="num_people" id="num_people" placeholder="Choose amount" required/>
 						</div>
 
 						<div class="fields-container fields-container-booking">
@@ -270,8 +256,8 @@ if (! class_exists('Design_My_Night')) {
 								<option default disabled>Choose Time</option>
 								<?php 
 										
-								foreach ($this->times as $time) {
-									?> <option value="<?= $time ?>"><?= $time ?></option> <?php
+								foreach ($this->times as $key => $time) {
+									?> <option value="<?= $this->times_server[$key] ?>"><?= $time ?></option> <?php
 								}
 	
 								?>
@@ -337,7 +323,12 @@ if (! class_exists('Design_My_Night')) {
 							padding: 0 !important;
 						}
 					</style>
-					<iframe id="form-iframe" src="<?php echo $this->url; ?>" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%" width="100%" height="100%" frameborder="0">
+					<iframe 
+						id="form-iframe" 
+						src="<?php echo $this->url; ?>" 
+						style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px"
+						height="100%"
+						width="100%" width="100%" height="100%" frameborder="0">
 					</iframe>
 				</div>
 			<?php
@@ -347,10 +338,36 @@ if (! class_exists('Design_My_Night')) {
 		}
 
 		/**
+		 * Every 15 Minutes, All Day Long
+		 * $range = hoursRange( 0, 86400, 60 * 15 );
+		 * 
+		 * Every 30 Minutes from 8 AM - 5 PM, using Custom Time Format
+		 * $range = hoursRange( 0, 86400, 60 * 30, 'h:i a' );
+		 */
+		function hoursRange( $lower = 0, $upper = 86400, $step = 3600, $format = '' ) {
+			$times = array();
+		
+			if ( empty( $format ) ) {
+				$format = 'g:i a';
+			}
+		
+			foreach ( range( $lower, $upper, $step ) as $increment ) {
+				$increment = gmdate( 'H:i', $increment );
+		
+				list( $hour, $minutes ) = explode( ':', $increment );
+		
+				$date = new DateTime( $hour . ':' . $minutes );
+		
+				$times[(string) $increment] = $date->format( $format );
+			}
+		
+			return $times;
+		}
+
+		/**
 		 * Shortcode handler for [private_hire]
 		 */
-		public function private_hire_form()
-		{
+		public function private_hire_form() {
 			$form = isset($_GET['form']) ? $_GET['form'] : '';
 			ob_start();
 			if (! isset($_GET['form'])) {
@@ -363,7 +380,9 @@ if (! class_exists('Design_My_Night')) {
 					$num_people = sanitize_text_field($_POST['num_people']);
 					$date = sanitize_text_field($_POST['date']);
 					$time = sanitize_text_field($_POST['time']);
-					// var_dump($date, $time, $venue_id, $event, $num_people);
+
+					$time_from = sanitize_text_field($_POST['time-from']);
+					$time_to = sanitize_text_field($_POST['time-to']);
 
 					if ($venue_id === '5e4bf7a9d4ea511f3f3feb33') {
 						$event = '5ea9f055eae53879de6205a7';
@@ -380,7 +399,8 @@ if (! class_exists('Design_My_Night')) {
 						'type' => $event,
 						'num_people' => $num_people,
 						'date' => $date,
-						'time' => $time
+						'time' => $time_from,
+						'duration' => $time_to
 					];
 
 					// Redirect to the submit confirmation page
@@ -427,26 +447,6 @@ if (! class_exists('Design_My_Night')) {
 							</select>
 						</div>
 						<div class="fields-container fields-container-hire">
-							<!-- Event -->
-							<label for="event">
-								<span>Event</span>
-								<svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path d="M13.3057 18.2975L8.23724 19.987C5.47183 20.9088 4.08912 21.3697 3.35924 20.6398C2.62936 19.9099 3.09026 18.5272 4.01207 15.7618L5.70156 10.6933C6.46758 8.39525 6.85059 7.24623 7.75684 7.03229C8.6631 6.81835 9.51953 7.67478 11.2324 9.38764L14.6114 12.7666C16.3242 14.4795 17.1807 15.3359 16.9667 16.2422" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-									<path d="M12.2351 18.3461C12.2351 18.3461 11.477 16.0649 11.477 14.5552C11.477 13.0454 12.2351 10.7643 12.2351 10.7643M8.06517 19.4833C8.06517 19.4833 7.42484 16.7314 7.307 14.9343C7.11229 11.965 8.06517 7.35254 8.06517 7.35254" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-									<path d="M14.5093 10.0061L14.6533 9.28614C14.7986 8.55924 15.3224 7.96597 16.0256 7.73155C16.7289 7.49714 17.2526 6.90387 17.398 6.17697L17.542 5.45703" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-									<path d="M17.5693 13.2533L17.7822 13.3762C18.4393 13.7556 19.2655 13.6719 19.8332 13.1685C20.3473 12.7126 21.0794 12.597 21.709 12.8723L22.0005 12.9997" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-									<path d="M9.79513 2.77903C9.45764 3.33109 9.54223 4.04247 9.99976 4.5L10.0976 4.59788C10.4908 4.99104 10.6358 5.56862 10.4749 6.10085" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
-									<path d="M6.92761 3.94079C7.13708 3.73132 7.47669 3.73132 7.68616 3.94079C7.89563 4.15026 7.89563 4.48988 7.68616 4.69935C7.47669 4.90882 7.13708 4.90882 6.92761 4.69935C6.71814 4.48988 6.71814 4.15026 6.92761 3.94079Z" fill="#fff" />
-									<path d="M12.1571 7.1571C12.3666 6.94763 12.7062 6.94763 12.9157 7.1571C13.1251 7.36657 13.1251 7.70619 12.9157 7.91566C12.7062 8.12512 12.3666 8.12512 12.1571 7.91566C11.9476 7.70619 11.9476 7.36657 12.1571 7.1571Z" fill="#fff" />
-									<path d="M17.1571 10.1571C17.3666 9.94763 17.7062 9.94763 17.9157 10.1571C18.1251 10.3666 18.1251 10.7062 17.9157 10.9157C17.7062 11.1251 17.3666 11.1251 17.1571 10.9157C16.9476 10.7062 16.9476 10.3666 17.1571 10.1571Z" fill="#fff" />
-									<path d="M19.0582 15.3134C19.2677 15.1039 19.6073 15.1039 19.8168 15.3134C20.0262 15.5228 20.0262 15.8624 19.8168 16.0719C19.6073 16.2814 19.2677 16.2814 19.0582 16.0719C18.8488 15.8624 18.8488 15.5228 19.0582 15.3134Z" fill="#fff" />
-									<path d="M19.3615 7.71436C18.6912 8.38463 19.1722 10.328 19.1722 10.328C19.1722 10.328 21.1156 10.809 21.7859 10.1387C22.4958 9.42884 22.0941 8.49708 21.0002 8.5C21.0032 7.40615 20.0714 7.00447 19.3615 7.71436Z" stroke="#fff" stroke-linejoin="round" />
-									<path d="M15.1884 3.41748L15.1608 3.51459C15.1305 3.62126 15.1153 3.67459 15.1225 3.72695C15.1296 3.77931 15.1583 3.82476 15.2157 3.91567L15.2679 3.99844C15.4698 4.31836 15.5707 4.47831 15.5019 4.60915C15.4332 4.73998 15.2402 4.75504 14.8544 4.78517L14.7546 4.79296C14.6449 4.80152 14.5901 4.8058 14.5422 4.83099C14.4943 4.85618 14.4587 4.89943 14.3875 4.98592L14.3226 5.06467C14.072 5.36905 13.9467 5.52124 13.8038 5.50167C13.6609 5.4821 13.595 5.30373 13.4632 4.94699L13.4291 4.85469C13.3916 4.75332 13.3729 4.70263 13.3361 4.66584C13.2993 4.62905 13.2486 4.61033 13.1472 4.57287L13.0549 4.53878C12.6982 4.40698 12.5198 4.34108 12.5003 4.19815C12.4807 4.05522 12.6329 3.92992 12.9373 3.67932L13.016 3.61448C13.1025 3.54327 13.1458 3.50767 13.1709 3.45974C13.1961 3.41181 13.2004 3.35699 13.209 3.24735L13.2168 3.14753C13.2469 2.76169 13.262 2.56877 13.3928 2.50001C13.5236 2.43124 13.6836 2.53217 14.0035 2.73403L14.0863 2.78626C14.1772 2.84362 14.2226 2.8723 14.275 2.87947C14.3273 2.88664 14.3807 2.87148 14.4873 2.84117L14.5845 2.81358C14.9598 2.70692 15.1475 2.65359 15.2479 2.75402C15.3483 2.85445 15.295 3.04213 15.1884 3.41748Z" stroke="#fff" />
-								</svg>
-							</label>
-							<input type="text" name="event" id="event" placeholder="Tell us about your party" >
-						</div>
-						<div class="fields-container fields-container-hire">
 							<!-- Guests -->
 							<label for="guests">
 								<span>Guests</span>
@@ -456,26 +456,43 @@ if (! class_exists('Design_My_Night')) {
 									<path fill-rule="evenodd" clip-rule="evenodd" d="M12.6939 12.6375C11.8834 12.2297 10.9679 12 10 12C6.68629 12 4 14.6863 4 18V20H16V18C16 17.6596 15.9716 17.3255 15.9169 17H20V14.75C20 12.6235 18.1515 11 16 11C14.6441 11 13.422 11.6366 12.6939 12.6375ZM13.9407 13.4754C14.5786 14.0315 15.0981 14.7205 15.4558 15.5H18.5V14.75C18.5 13.5628 17.4384 12.5 16 12.5C15.1288 12.5 14.3841 12.8975 13.9407 13.4754ZM10 13.5C7.51472 13.5 5.5 15.5147 5.5 18V18.5H14.5V18C14.5 17.4727 14.4096 16.9681 14.2441 16.4999C13.9244 15.5953 13.3224 14.8219 12.5431 14.2869C11.8199 13.7905 10.945 13.5 10 13.5Z" fill="#fff" />
 								</svg>
 							</label>
-							<input type="number" name="num_people" id="num_people" required/>
+							<input type="number" name="num_people" id="num_people" placeholder="Choose amount" required/>
 						</div>
 
 						<div class="fields-container fields-container-hire">
-							<label for="time">
-								<span>Time</span>
+							<label for="time-from">
+								<span>From</span>
 								<svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 								</svg>
 							</label>
-							<select required name="time" required>
+							<select required name="time-from" required>
 								<option default disabled>Choose Time</option>
 								<?php 
 										
-								foreach ($this->times as $time) {
-									?> <option value="<?= $time ?>"><?= $time ?></option> <?php
+								foreach ($this->times as $key => $time) {
+									?> <option value="<?= $this->times_server[$key] ?>"><?= $time ?></option> <?php
 								}
 	
 								?>
-								<!-- <option value="3">10:00</option> -->
+							</select>
+						</div>
+						<div class="fields-container fields-container-hire">
+							<label for="time-to">
+								<span>To</span>
+								<svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+								</svg>
+							</label>
+							<select required name="time-to" required>
+								<option default disabled>Choose Time</option>
+								<?php 
+										
+								foreach ($this->times as $key => $time) {
+									?> <option value="<?= $this->times_server[$key] ?>"><?= $time ?></option> <?php
+								}
+	
+								?>
 							</select>
 						</div>
 						<button type="submit" name="hire_btn" id="hire_btn">Enquire Now</button>
@@ -505,12 +522,17 @@ if (! class_exists('Design_My_Night')) {
 				$num_people = $_GET['num_people'];
 				$time = $_GET['time'];
 
+				$time_from = $_GET['time'];
+				$time_to = 'number:' . round(abs(strtotime($_GET['duration']) - strtotime($_GET['time'])) / 60,2);
+
 				// Construct the URL with the parameters
 				$this->url = "https://bookings.designmynight.com/book?venue_id=" . urlencode($venue_id) 
 				. "&source=patner&date=" . urlencode($date) 
 				. "&type=" . urlencode($type) 
 				. "&num_people=" . urlencode($num_people) 
-				. "&time=" . urlencode($time);
+				. "&time=" . urlencode($time_from)
+				. "&duration=" . urlencode($time_to);
+
 			?>
 				<div class="iframe-container">
 					<iframe id="form-iframe" src="<?php echo $this->url; ?>" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%" width="100%" height="100%" frameborder="0">
